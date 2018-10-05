a891a408287538becca04911d77afe1caa2b5430
ralph <ralphjohn292@gmail.com>
2018-10-05 15:54:32 +0800

    Ralph Code

--- app/Comment.php
+++ app/Comment.php
@@ -8,12 +8,15 @@ class Comment extends Model
 {
     protected $table = 'xe_comments';
     protected $primaryKey = 'comment_srl';
-    public $timestamps = false;
 
 
     protected $fillable = [
-        'content', 'nick_name', ''
+        'content', 'nick_name', 'user_name','member_srl', 'module_srl', 'document_srl', 'email_address', 'ipaddress'
     ];
+
+    public function posts(){
+        return $this->belongsTo('App\Post');
+    }
 
     public function posts(){
         return $this->belongsTo('App\Post');
